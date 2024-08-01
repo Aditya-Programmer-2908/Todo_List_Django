de5a522d7c2a942f6f8b3d807debe5d117e444e7
Aditya Pratap Singh <adityapratap.usingh@gmail.com>
2024-08-01 12:23:54 +0530

    commit

--- todo_list/base/views.py
+++ todo_list/base/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render
+from django.forms import BaseModelForm
+from django.shortcuts import render,redirect
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from .models import Task
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView,FormView
 from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 from django.http import HttpResponse
@@ -18,10 +21,44 @@ class CustomLoginView(LoginView):
 
     def get_success_url(self):
         return reverse_lazy('tasks')
+    
+class RegisterPage(FormView):
+    template_name='base/register.html'
+    form_class=UserCreationForm
+    redirect_authenticate_user=True
+    success_url=reverse_lazy('tasks')
 
-class TaskList(LoginRequiredMixin,ListView):
+    def form_valid(self, form):
+        user=form.save()
+        if user is not None:
+            login(self.request,user)
+        return super(RegisterPage,self).form_valid(form)
+    
+    def get(self,request,*args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('tasks')
+        return super(RegisterPage,self).get(request, *args, **kwargs)
+    
+
+
+
+class TaskList(LoginRequiredMixin, ListView):
     model = Task
-    context_object_name='tasks'
+    context_object_name = 'tasks'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tasks'] = context['tasks'].filter(user=self.request.user)
+        context['count'] = context['tasks'].filter(complete=False).count()
+
+        search_input = self.request.GET.get('search-area') or ''
+        if search_input:
+            context['tasks'] = context['tasks'].filter(title__icontains=search_input)
+
+        context['search_input'] = search_input
+
+        return context
+
 
 class TaskDetail(LoginRequiredMixin,DetailView):
     model=Task
@@ -30,8 +67,12 @@ class TaskDetail(LoginRequiredMixin,DetailView):
 
 class TaskCreate(LoginRequiredMixin,CreateView):
     model=Task
-    fields='__all__'
+    fields=['title', 'description','complete']
     success_url=reverse_lazy('tasks')
+
+    def form_invalid(self, form):
+        form.instance.user=self.request.user
+        return super(TaskCreate, self).form_valid(form)
     
 class TaskUpdate(LoginRequiredMixin,UpdateView):
     model = Task
